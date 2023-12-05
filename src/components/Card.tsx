d695f0cd2dde49cdf6b0b5dd0d820f4ed96fa2f4
gruvector <vector.gru316@gmail.com>
2023-12-05 16:04:47 +0900

    quiz_card1

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,12 +1,22 @@
 import React from "react";
 
-const Card = () => {
+interface CardPros {
+    quiztitle: string,
+    quizsentence: String
+
+}
 
 
+const Card = (props: CardPros) => {
 
     return (
         <div>
-            
+            <div className="card_quiztype">質問タイトル質問タイトル</div>
+            <div className="card_quiztitle">{props.quiztitle}</div>
+            <div className="card_quizsentence">{props.quizsentence}</div>
+            <div className="card_textfield">
+                <textarea name="" id="" cols={30} rows={10} maxLength={2000} placeholder="回答を入力してください"></textarea>
+            </div>
         </div>
     )
 }
--- src/components/CheckboxCard.tsx
+++ src/components/CheckboxCard.tsx
@@ -2,8 +2,6 @@ import React from "react";
 
 const CheckboxCard = () => {
 
-
-
     return (
         <div>
             
